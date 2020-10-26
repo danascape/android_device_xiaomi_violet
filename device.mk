5bdb253de55d2c2979e6ef3e578681117edd6ce8
iamsaalim <saalimquadri1@gmail.com>
2020-10-26 11:21:24 +0000

    violet: Build Snap
    Change-Id: I071d2996f434ee1cb34abb428a2e954d314e573e

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
