473513f2aeaf37e7b88c84a91ef1f287bab25235
danascape <danascape@gmail.com>
2021-10-04 16:34:00 +0530

    violet: Import Wifi configs from violet-user 10 QKQ1.190915.002 V12.5.1.0.QFHCNXM release-keys
    Change-Id: Iab4cf775fcc813111dfc391ab0c089c57aa4f05a

--- device.mk
+++ device.mk
@@ -163,6 +163,12 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     vndk_package
 
+# Wifi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl
